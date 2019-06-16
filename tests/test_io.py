a0a24a9e255c440ec041150a4c7e79bda5f60c73
Jessica Sousa <jessicacardosops@gmail.com>
2019-06-16 02:40:47 -0300

    Adicionar método test_load_prefixes.

--- tests/test_io.py
+++ tests/test_io.py
@@ -10,3 +10,12 @@ def test_load_templates():
     )
     obj_type = "pandas.core.frame"
     assert type(QApedia.io.load_templates(filepath)).__module__ == obj_type
+
+
+def test_load_prefixes():
+    filepath = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'fixtures/prefixes.txt',
+    )
+    assert type(QApedia.io.load_prefixes(filepath)) == tuple
+
